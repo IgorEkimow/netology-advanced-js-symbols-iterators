894e82495ca6a8f8f645467a06f2e3d7b617c05c
Igor <igorekimow@gmail.com>
2021-03-14 16:24:38 +0300

    Done

--- src/js/Character.js
+++ src/js/Character.js
@@ -2,7 +2,7 @@ export default class Character {
   constructor(name) {
     this.name = name;
     this.type = 'Bowman';
-    this.health = 50;
+    this.health = 100;
     this.level = 1;
     this.attack = 40;
     this.defence = 10;
--- src/js/__tests__/Team.test.js
+++ src/js/__tests__/Team.test.js
@@ -1,7 +1,7 @@
 import Team from '../Team';
 import Character from '../Character';
 
-test('shoul test Team', () => {
+test('Expect to form the Team', () => {
   const team = new Team();
   team.add(new Character('Alex'));
   team.add(new Character('Ben'));
@@ -9,11 +9,11 @@ test('shoul test Team', () => {
   team.add(new Character('John'));
   team.add(new Character('Anna'));
   team.add(new Character('Elizabeth'));
-  for (const pers of team) {
-    expect(pers).toEqual({
-      name: pers.name,
+  for (const character of team) {
+    expect(character).toEqual({
+      name: character.name,
       type: 'Bowman',
-      health: 50,
+      health: 100,
       level: 1,
       attack: 40,
       defence: 10,
